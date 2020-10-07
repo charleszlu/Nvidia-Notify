5d39cc33da250834e5e6f54907363fa266c33aa5
Charles Lu <mail@charleslu.net>
2020-10-08 08:36:48 +1100

    Add grace period for alert

--- notifier.py
+++ notifier.py
@@ -7,7 +7,7 @@ import requests
 # import webbrowser
 from time import sleep
 import random
-from datetime import datetime, time
+from datetime import datetime, time, timedelta
 from dotenv import load_dotenv
 from os import path, getenv
 
@@ -16,6 +16,10 @@ from twilio.rest import Client
 GET_SELENIUM = 0
 GET_URLLIB = 1
 GET_API = 2
+
+getenv('REPEAT_ALERT_AFTER_MIN') 
+
+IN_STOCK_ALERT = dict()
 
 
 ### CONFIG SECTION BELOW ------------------------------------------------------
@@ -38,8 +42,8 @@ NOTIFY_MAC = False
 NOTIFY_WIN = False
 
 urlKeyWords = {
-    "https://www.nvidia.com/en-us/geforce/graphics-cards/30-series/rtx-3080/" : ("https://api-prod.nvidia.com/direct-sales-shop/DR/products/en_us/USD/5438481700", False, GET_API, 'Nvidia 3080', ),
-    # "https://www.nvidia.com/en-us/geforce/graphics-cards/30-series/rtx-3090/" : ("https://api-prod.nvidia.com/direct-sales-shop/DR/products/en_us/USD/5438481600,5443202600", False, GET_API, 'Nvidia 3090', ),
+    "https://www.nvidia.com/en-us/geforce/graphics-cards/30-series/rtx-3080/" : ("https://api-prod.nvidia.com/direct-sales-shop/DR/products/en_us/USD/5438481700", False, GET_API, 'Nvidia 3080'),
+    # "https://www.nvidia.com/en-us/geforce/graphics-cards/30-series/rtx-3090/" : ("https://api-prod.nvidia.com/direct-sales-shop/DR/products/en_us/USD/5438481600,5443202600", False, GET_API, 'Nvidia 3090'),
     # "https://www.evga.com/products/productlist.aspx?type=0&family=GeForce+30+Series+Family&chipset=RTX+3080" : ("AddCart", True, GET_URLLIB, 'EVGA 3080'),
     # "https://www.evga.com/products/productlist.aspx?type=0&family=GeForce+16+Series+Family&chipset=GTX+1650+Super" : ("AddCart", True, GET_URLLIB, 'EVGATest'),
     # "https://www.newegg.com/p/pl?d=rtx+3080&N=100007709%20601357247" : ("Add to cart", True, GET_URLLIB, 'Newegg 3080'),
@@ -82,8 +86,19 @@ def test_alert():
     print("Sending test notifier...")
     message = client.messages.create(to=twilioToNumber, from_=twilioFromNumber, body='nVidia Notifier started running. Alerts will be sent from this number.')
 
+def clean_alert_record():
+    for product in IN_STOCK_ALERT:
+        if IN_STOCK_ALERT[product]+timedelta(minutes=int(getenv('REPEAT_ALERT_AFTER_MIN'))) > datetime.now():
+            del IN_STOCK_ALERT[product]
+
 def alert(url):
     product = urlKeyWords[url][3]
+
+    # Check if needs to alert
+    if product in IN_STOCK_ALERT and IN_STOCK_ALERT[product]+timedelta(minutes=int(getenv('REPEAT_ALERT_AFTER_MIN'))) > datetime.now():
+        print("{} ALREADY RECENTLY ALERTED".format(product))
+        return
+
     print("{} IN STOCK".format(product))
     print(url)
     # webbrowser.open(url, new=1)
@@ -188,7 +203,9 @@ def main():
         baseSleepAmt = 1
         totalSleep = baseSleepAmt + random.uniform(int(getenv('REFRESH_TIME_MIN')), int(getenv('REFRESH_TIME_MAX')))
         # print("Sleeping for {} seconds".format(totalSleep))
+        clean_alert_record()
         sleep(totalSleep)
+        
 
 
 if __name__ == '__main__':
